802928fd736c5710bbea6af93e91fccd61450600
John Doe <toukan.amer@gmail.com>
2018-08-19 09:09:35 -0400

    fully operatable

--- bamazon.sql
+++ bamazon.sql
@@ -11,6 +11,6 @@ primary key (item_id)
 );
 
 INSERT INTO products (product_name, dept_name, price, stock_qty) VALUES ("iPhone","Electronics", 799.99 , 
-30 ), ('Coffee Beans','Food & Beverages', 20, 100), ('Sunglasses', 'Eye Wear', 39.99, 40),('Laptop', 'Electronics', 399.99, 70), ('Deodorant', 'Hygeine', 12, 150), ('Toothpaste', 'Hygeine', 18.99, 600), ('Gatorade' , 'Food & Beverages', 6.99, 100), ('Highlighter', 'office supplies', 2.99, 1000), ('Pen' , 'office supplies' , 2.99 , 1000), ('Water', 'Food & Beverages', 1.99 ,30000);
+30) , ('Coffee Beans','Food & Beverages', 20, 100), ('Sunglasses', 'Eye Wear', 39.99, 40),('Laptop', 'Electronics', 399.99, 70), ('Deodorant', 'Hygeine', 12, 150), ('Toothpaste', 'Hygeine', 18.99, 600), ('Gatorade' , 'Food & Beverages', 6.99, 100), ('Highlighter', 'office supplies', 2.99, 1000), ('Pen' , 'office supplies' , 2.99 , 1000), ('Water', 'Food & Beverages', 1.99 ,30000);
 SELECT * FROM products;
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,21 +1,103 @@
 require('dotenv').config();
+
 var mysql = require ('mysql');
+var inquirer = require ('inquirer');
 var connection = mysql.createConnection(
     {
     host: process.env.host,
     port: process.env.port,
     user: process.env.user,
     password: process.env.password,
-    database:process.env.database
+    database: process.env.database
     });
-var inquirer = require ('inquirer');
-connection.connect(function(err){
-    if (err) console.log('Connection Failed.');
-    console.log('connected'+connection.threadId);
-});
 
-connection.query('SELECT item_id, product_name, dept_name, price FROM products', function (err,res){
-    if(err) console.log('failed to select query');
-    console.log(res);
-    connection.end();
+
+function validateInput(value){
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
+
+    if (integer && (sign == 1)){
+        return true;
+    } else {
+        return 'Please enter a whole number.';
+    }
+}
+
+function inquire(){
+    inquirer.prompt([
+    {
+        name: 'item_id',
+        type: 'input',
+        message: 'Please enter the item ID of the item you would like to purchase.',
+        validate: validateInput, 
+        filter: Number
+        },
+        {
+        name : "quantity",
+        type: 'input',
+        message: 'Please enter the quantity.',
+        validate: validateInput, 
+        filter: Number
+        }
+    ]).then (function(answer){
+        var quantity = answer.quantity;
+        var item = answer.item_id;
+
+        //query db to confirm the given item exists in the desired quantity
+        var queryStr = 'SELECT * FROM products WHERE ?';
+        connection.query(queryStr, {item_id: item}, function (err, res){
+        if (err) throw err; 
+        if (res.length === 0){
+            console.log('ERROR: please enter a valid item ID');
+        } else {
+            var productData = res[0];
+
+            //If Product is available
+    if (quantity <= productData.stock_qty){
+        console.log('Good news, your product is in stock! Placing order ...');
+var qty = productData.stock_qty - quantity;  
+var updateStock = 'UPDATE products SET stock_qty = ' + qty+ ' WHERE item_id= ' +item ;
+
+ connection.query(updateStock, {stock_qty:qty} , function(err, res){
+        if (err) throw err; 
+        var total = productData.price * quantity;
+        
+        console.log('Your order has been placed, Your total is ');
+        console.log(total);
+        console.log('Thank you for shopping with us! See you soon');
+
+        // //END CONNECTION
+         connection.end();
+    });
+    } else {
+        console.log('Sorry, seems like the product your trying to order is running low on stock.');
+        console.log('This means we will not be able to fulfill your order with the quantity requested. Please try again with a different quantity');
+
+        store();
+    }
+        }
+        });
+    });
+}
+
+function store(){
+    queryString = 'SELECT item_id, product_name, dept_name, price FROM products';
+connection.query(queryString, function (err,res){
+   if(err) throw err; 
+
+   console.log('\n\nWELCOME TO BAMAZON!! HERE IS OUR INVENTORY LIST: ');
+   console.log('---------------------------------------\n\n');
+
+   var strOut = '';
+
+   for (var i = 0; i < res.length ; i++){
+    console.log("\n\nProduct ID: " + res[i].item_id + "\nProduct : " + res[i].product_name + "\nDepartment: " + res[i].dept_name + "\nPrice: " + res[i].price + '\n\n-----------------------------' );
+    }
+    console.log ('----------------------------------------\n\n');
+    inquire();
 });
+}
+
+
+
+store();
